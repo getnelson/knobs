42ce55fa24296931944b529916dc3b4135349a93
Timothy Perrett <tperrett@lyft.com>
2018-10-14 20:13:09 -0700

    Fix tests

--- .buildkite/pipeline.exec.sh
+++ .buildkite/pipeline.exec.sh
@@ -9,7 +9,7 @@ export TRAVIS_COMMIT="$BUILDKITE_COMMIT"
 export TRAVIS_REPO_SLUG="getnelson/knobs"
 export TRAVIS_JOB_NUMBER="1.1"
 export TRAVIS_BUILD_NUMBER="$BUILDKITE_BUILD_NUMBER"
-export KNOBS_TEST_DIR="/tmp"
+export KNOBS_TEST_DIR="knobs-test"
 
 if [ "$BUILDKITE_PULL_REQUEST" = 'false' ]; then
 	git checkout -qf "$BUILDKITE_BRANCH";
