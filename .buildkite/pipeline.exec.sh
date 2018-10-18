85da3c5138dcfcf864d795d7e5c6207e656e35f6
Timothy Perrett <tperrett@lyft.com>
2018-10-18 13:21:25 -0700

    Publish 2.11 and 2.12

--- .buildkite/pipeline.exec.sh
+++ .buildkite/pipeline.exec.sh
@@ -7,7 +7,6 @@ git config --global user.name "Nelson Team"
 export TRAVIS="true" # way hacky
 export TRAVIS_COMMIT="$BUILDKITE_COMMIT"
 export TRAVIS_REPO_SLUG="getnelson/knobs"
-export TRAVIS_JOB_NUMBER="1.1"
 export TRAVIS_BUILD_NUMBER="$BUILDKITE_BUILD_NUMBER"
 export KNOBS_TEST_DIR="knobs-test"
 
@@ -15,4 +14,4 @@ if [ "$BUILDKITE_PULL_REQUEST" = 'false' ]; then
 	git checkout -qf "$BUILDKITE_BRANCH";
 fi
 
-sbt ++2.11.11 'release with-defaults'
+sbt "++${TRAVIS_SCALA_VERSION}" 'release with-defaults'
